e95ef0be7e49a675278cecffb44eda4896b13106
salonicodes <45900324+salonicodes@users.noreply.github.com>
2019-11-17 10:15:11 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
   author = 'Saloni Tripathi, Bhaskar Tak, Govind Sisodia, Aditi Bambodi',                   # Type in your name
   author_email = 'engineersaloni@gmail.com',      # Type in your E-Mail
   url = 'https://github.com/salonicodes/anacom',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/salonicodes/mylib/archive/v_01.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/salonicodes/anacom/archive/v_01.tar.gz',    # I explain this later on
   keywords = ['Analog Communication', 'Modulation', 'Fourier'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
           'sympy',
